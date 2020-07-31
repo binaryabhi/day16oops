5bad4094b831d622a9cbec68608c7e45b69fd3d5
binaryabhi <abhinavkishorem9@gmail.com>
2020-07-31 17:22:54 +0530

    Committed

---
+++ forloopprob5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+echo "enter any number to find factorial "
+read num
+a=0
+factorial=1
+for (( counter=$num; $counter-gt$a; counter-- ))
+do
+factorial=$(( $factorial * $counter ))
+done
+echo $factorial
+
